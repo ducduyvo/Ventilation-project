66f704d2b3ba45dfaf704152d4c56c3348598801
ducmavuong <ducvoty@gmail.com>
2020-03-17 13:45:49 +0200

    checking to get the i2c pressure

--- src/Pressure.cpp
+++ src/Pressure.cpp
@@ -1,19 +1,21 @@
 #include "Pressure.h"
 #include <mutex>
 
-Pressure::Pressure() : i2c(i2c_config) { }
+Pressure::Pressure() : i2c(i2c_config) {}
 
 void Pressure::updatePressure()
 {
     uint8_t data[3];
     uint8_t cmd = READ_COMMAND;
     int16_t value = 0;
-    i2c.transaction(PRESSURE_ADDRESS, &cmd, 1, data, 3);
-    /* i2c.write(PRESSURE_ADDRESS, &cmd, 1); */
-    /* i2c.read(PRESSURE_ADDRESS, data, 3); */
-
-    value = (data[0] << 8) | data[1];
-    pressure = convertPressure(value);
+    if (i2c.transaction(PRESSURE_ADDRESS, &cmd, 1, data, 3))
+    {
+        value = (data[0] << 8) | data[1];
+        pressure = convertPressure(value);
+    }
+    else{
+        printf("Error in getting pressure");
+    }
 }
 
 int16_t Pressure::convertPressure(int16_t pressure_)
@@ -21,8 +23,8 @@ int16_t Pressure::convertPressure(int16_t pressure_)
     return (pressure_ / SCALE_FACTOR) * 0.95;
 }
 
-
-int16_t Pressure::getPressure() {
-   // std::lock_guard<Imutex> lock(guard);
+int16_t Pressure::getPressure()
+{
+    // std::lock_guard<Imutex> lock(guard);
     return pressure;
 }
